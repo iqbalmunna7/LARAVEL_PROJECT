fe06945dff868526ffae9f043306043bbbe89f7d
iqbalmunna7 <iqbal.munna678@gmail.com>
2022-02-03 17:24:28 +0600

    LARAVEL_PROJECT FOLDER UPLOAD

--- day-15/app/classes/Example.php
+++ day-15/app/classes/Example.php
@@ -5,8 +5,34 @@ namespace App\classes;
 
 class Example
 {
+
+
+    //public $firstNumber = 10;
+    //public $firstName ="iqbal";
+    //public $lastName ="munna";
+    public $firstNumber;
+    public $firstName ;
+    public $lastName;
+
+
     public function index ()
     {
-        echo "hello world";
+
+        echo $this->firstNumber ="20";
+        $this->firstName ="IQBAL";
+        $this->lastName ="MUNNA";
+
+        // echo $this->firstNumber;
+        // echo $this->firstName;
+        // echo "hello world"
+        // echo $this->lastName;
+
+       echo '<br/>';
+       echo $this->firstName;
+       echo'<br/>.';
+       echo $this->lastName;
+       echo '<br/>';
+       echo $this->firstName.$this->lastName;
+
     }
 }
--- day-15/index.php
+++ day-15/index.php
@@ -4,3 +4,15 @@ use  App\classes\Example;
 
 $example = new Example();
 $example->index();
+/*
+ * 3  major  rules  for  variable
+ *  start with $ doller  sign
+ * A-Z a-z 0-9 _()
+ * no  numbers  in first
+ * * stantdard  rule  for  variable   naming
+ * start with  samll  letter
+ * meaningful
+ * readable
+ *
+ *
+ */
