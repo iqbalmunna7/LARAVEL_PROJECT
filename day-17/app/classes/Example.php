adaaf1c5ed0881a9956a093bc26fea3dee3580ae
iqbal.munna7 <iqbal.munna678@gmail.com>
2022-02-07 17:39:19 +0600

    LARAVEL_PROJECT FOLDER upload

---
+++ day-17/app/classes/Example.php
@@ -0,0 +1,45 @@
+<?php
+namespace  App\classes;
+class Example
+{
+    public $i;
+    public function  index()
+    {
+        /*
+         * SINGLE LINE
+         * conditional
+         * if
+         * if else
+         * if else if
+         * switch
+         * repeated
+         * for
+         *  while
+         * do while
+         *  foreach
+         * //echo 'Hello World';
+          for ($this->i =50; $this->i >22; $this->i--)
+        {
+            echo $this->i.'<br/>';
+        }
+         //
+
+        //
+         $this->i=10;
+        while ($this->i <20)
+        {
+            echo $this->i.'<br/>';
+            $this->i++;
+        }
+        * */
+$this->i =10;
+do{
+    echo 4this->i;
+    $this->i++;
+
+}while($this->i >20);
+
+
+    }
+
+}
